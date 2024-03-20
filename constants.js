fdbd30a795431b3f75203c9dc46d71cb3205c4fb
HectorSaldes <hsaldana0516@mor.conalep.edu.mx>
2024-03-20 15:08:08 -0600

    :art: new section projects

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-// export const API_FORM_URL = 'https://contacts-nunna.up.railway.app/api/v1';
-export const API_FORM_URL = 'http://localhost:3000/api/v1';
+export const API_FORM_URL = 'https://contacts-nunna.up.railway.app/api/v1';
+// export const API_FORM_URL = 'http://localhost:3000/api/v1';
